e2cc3d488275c26f4d5b70c49b48c8558b996ee6
marghherita <38952106+marghherita@users.noreply.github.com>
2024-05-28 12:16:34 +0200

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="bottom-0  border-t-2 py-6 text-center border-primary lg:fixed md:fixed  h-fit w-full text-[10px] text-cardHover">
+    <div className="bottom-0  border-t-2 py-6 text-center border-primary bg-primary lg:fixed md:fixed  h-fit w-full text-[10px] text-secondary">
       Copyright © {year} Linker Digital. Tutti i diritti riservati.
     </div>
   );
