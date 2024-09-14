7183dd9cb970d73127ed2fedb2cb5b509fb02b4e
marghherita <38952106+marghherita@users.noreply.github.com>
2024-09-14 11:57:13 +0200

    update clarity script

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,29 +1,30 @@
 // pages/_document.tsx
 
-import { Html, Head, Main, NextScript, DocumentProps } from 'next/document';
+import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
 
 export default function MyDocument() {
-    return (
-        <Html>
-            <Head>
-                {/* Clarity Tracking Code */}
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "ng0f7e5t3p");
+  return (
+    <Html>
+      <Head>
+        {/* Clarity Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "o31336ewh2");
+                    
             `,
-                    }}
-                />
-                {/* Fine codice Clarity */}
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+          }}
+        />
+        {/* Fine codice Clarity */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
